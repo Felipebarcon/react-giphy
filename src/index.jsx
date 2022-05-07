3c726be98ead4ce28ecd20aedecec48ab278c36d
Felipe Barcon <felipe.barcon@gmail.com>
2022-05-07 15:47:28 +0200

    first div Hello World

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
+
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+  }
+
+  render() {
+    // Build and return HTML
+    return (
+      <div className={this.state.clicked ? 'clicked' : null}>
+        Hello, {this.props.name}
+      </div>
+    );
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
